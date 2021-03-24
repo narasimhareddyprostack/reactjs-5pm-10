93e73e17da1b2a3b09d478614e74042ac8e8dc84
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-03-24 17:57:12 +0530

    git psuh

---
+++ JS/Basics/DataTypes.js
@@ -0,0 +1,28 @@
+var emp_No = 101;
+var emp_Name = "Kavitha";
+var is_Avail = true;
+var sal;
+
+var size_Shirt = [38, 40, 42, 44];
+var employee = { name: "Kavitha", age: 24, loc: "Bangalore" };
+//how to find data type?  typeof
+
+console.log(typeof emp_No);
+console.log(typeof emp_Name);
+
+console.log(typeof is_Avail);
+console.log(typeof sal);
+console.log(typeof size_Shirt);
+console.log(typeof employee);
+
+function abc() {}
+console.log(typeof abc);
+
+/*  identifier - variable, function, class, object
+    identifier rules
+    1. identifier should start with  - a to z or A to Z
+       1.1 . identier should start with _ and $
+    2. should not start with 0 - 9 
+    3. should not contains special chars/symbols
+    4. should not use keywords/reserved words
+*/
---
+++ JS/Basics/DataTypesone.js
@@ -0,0 +1,8 @@
+var if = 100;
+
+function void() {}
+
+//we cant use keywords as identifier
+
+var _emp_No = 100;
+var $emp_Name = "Kavitha";
